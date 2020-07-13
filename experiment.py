a498df5bdb434ff8c2db32f40afc98e350e31ba4
Daniel Lengyel <daniel.lengyel@berkeley.edu>
2020-07-13 14:37:10 +0200

    prepared for gpu

--- experiment.py
+++ experiment.py
@@ -48,45 +48,45 @@ if config["net_name"] == "SimpleNet":
 elif config["net_name"] == "LeNet":
     config["net_params"] = [height, width, num_channels, out_dim]
 
-config["seed"] = 1
+config["seed"] = 0
 
-config["num_steps"] = 400  # tune.grid_search([25000]) # roughly 50 * 500 / 16
+config["num_steps"] = 50000  # tune.grid_search([25000]) # roughly 50 * 500 / 16
 config["mean_loss_threshold"] = None # 0.15
 
-config["batch_train_size"] = tune.grid_search([ 8, 16])
+config["batch_train_size"] = tune.grid_search([32, 256, 4096])
 config["batch_test_size"] = 16 # tune.grid_search([16])
 
 config["var_noise"] = None # tune.grid_search(list(np.logspace(-4, -1, 5)))
 
 config["ess_threshold"] = None  # tune.grid_search([0.97])
-config["sampling_tau"] = 5 # tune.grid_search([1, 5]) # tune.grid_search([1, 25, 100])
+config["sampling_tau"] = 1 # tune.grid_search([1, 5]) # tune.grid_search([1, 25, 100])
 config["sampling_wait"] = 0
 config["sampling_stop"] = None
 config["weight_type"] = "loss_gradient_weights"  # "input_output_forbenius", #
 
-config["learning_rate"] = 0.1 # tune.grid_search(list(np.logspace(-3, 0, 10))) # 0.1 # tune.grid_search([0.1]) # 1 # tune.grid_search(list(np.logspace(-2, 1, 10)))
+config["learning_rate"] = 0.1 # tune.grid_search(list(np.logspace(-3, 0, 10))) 
 # config["lr_decay"] =
 config["momentum"] = 0
 
-config["num_nets"] = 1  # would like to make it like other one, where we can define region to initialize
+config["num_nets"] = 2  # would like to make it like other one, where we can define region to initialize
 
 config["softmax_beta"] = 0 # tune.grid_search([-50, 50]) #tune.grid_search([0] + list(-1*np.linspace(1, 100, 5)) + list(np.linspace(1, 100, 5))) # e.g. negtive to prioritize low weights
 # offset = tune.grid_search([0.5, 0.25, 0.1])
 config["softmax_adaptive"] = None  # [offset, 1000] # offset, and strength
 
 
-config["device"] = "cpu"
+config["device"] = "gpu"
 
 config["hard_train_eps"] = None #  0.1
 
 if data_name == "MNIST":
     config["reduce_train_per"] = 0.1
 else:
-    config["reduce_train_per"] = 0.1
+    config["reduce_train_per"] = 1
 
 # --- Set up folder in which to store all results ---
 folder_name = get_file_stamp()
-cwd = os.environ["PATH_TO_FLAT_FOLDER"]
+cwd = os.environ["PATH_TO_GG_FOLDER"]
 folder_path = os.path.join(cwd, "experiments", data_name, folder_name)
 print(folder_path)
 os.makedirs(folder_path)
